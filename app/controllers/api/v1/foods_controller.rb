5342384d96e9902d7ac4abc707c82fc8955c159f
agpiermarini <piermarini.andrew@gmail.com>
2018-06-25 19:49:26 -0600

    Add food show endpoint. Add ActiveRecord::RecordNotFound rescue method to ApplicationController

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::FoodsController < ApplicationController
   def index
     render json: Food.all
   end
+
+  def show
+    food = Food.find(params[:id])
+    if food
+      render json: food
+    else
+      render status: :not_found
+    end
+  end
 end
--- spec/requests/api/v1/records/foods_spec.rb
+++ spec/requests/api/v1/records/foods_spec.rb
@@ -1,16 +1,43 @@
 require 'rails_helper'
 
 describe 'Food Endpoints' do
-  describe '/api/v1/foods' do
+  describe 'GET /api/v1/foods' do
     it 'returns all database entries for foods' do
-      create_list(:food, 5)
+      food_list = create_list(:food, 3)
 
       get '/api/v1/foods'
 
-      foods = JSON.parse(response.body)
+      foods = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_success
-      expect(foods.length).to eq(5)
+      expect(foods.length).to eq(3)
+      expect(foods.first[:name]).to eq(food_list.first.name)
+      expect(foods.first[:calories]).to eq(food_list.first.calories)
+      expect(foods.second[:name]).to eq(food_list.second.name)
+      expect(foods.second[:calories]).to eq(food_list.second.calories)
+      expect(foods.last[:name]).to eq(food_list.last.name)
+      expect(foods.last[:calories]).to eq(food_list.last.calories)
+    end
+  end
+
+  describe 'GET /api/v1/foods/:id' do
+    it 'returns record for food object corresponding to the provided id' do
+      food_item = create(:food)
+
+      get "/api/v1/foods/#{food_item.id}"
+
+      food = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_success
+      expect(food[:name]).to eq(food_item.name)
+      expect(food[:calories]).to eq(food_item.calories)
+    end
+
+    it 'returns a 404 if there is no food object corresponding to the provided id' do
+      get "/api/v1/foods/1"
+
+      expect(response).to_not be_success
+      expect(response.status).to eq(404)
     end
   end
 end
