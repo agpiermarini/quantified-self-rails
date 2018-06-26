564eeee88354417eeb253cb81735d28730000c84
agpiermarini <piermarini.andrew@gmail.com>
2018-06-26 17:30:28 -0600

    Add routes for meal foods. Add meal_foods endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods
+      resources :meals, only: [:index] do
+        get 'foods',          to: 'meal_foods#index'
+        get 'foods/:food_id', to: 'meal_foods#show'
+      end
     end
   end
 end
--- spec/requests/meal_spec.rb
+++ spec/requests/meal_spec.rb
@@ -11,6 +11,29 @@ describe 'Meal Endpoints' do
 
       expect(response).to be_success
       expect(meals.length).to eq(3)
+      expect(meals[0][:name]).to eq(meal_list[0].name)
+      expect(meals[1][:name]).to eq(meal_list[1].name)
+      expect(meals[2][:name]).to eq(meal_list[2].name)
+      expect(meals[2][:foods]).to be_an(Array)
+      expect(meals[2][:foods].length).to be(3)
+    end
+  end
+
+  describe 'GET /api/v1/meals/:meal_id/foods' do
+    it 'returns all database entries for meals' do
+      meal = create(:meal)
+
+      get "/api/v1/meals/#{meal.id}/foods"
+
+      foods = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_success
+      expect(foods[0][:name]).to eq(meal.foods[0].name)
+      expect(foods[0][:calories]).to eq(meal.foods[0].calories)
+      expect(foods[1][:name]).to eq(meal.foods[1].name)
+      expect(foods[1][:calories]).to eq(meal.foods[1].calories)
+      expect(foods[2][:name]).to eq(meal.foods[2].name)
+      expect(foods[2][:calories]).to eq(meal.foods[2].calories)
     end
   end
 end
