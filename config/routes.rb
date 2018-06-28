8aaf749e8a30ddfaf2c19ba98971e21cdea71a7f
agpiermarini <piermarini.andrew@gmail.com>
2018-06-27 20:28:54 -0600

    Reconfigure meal foods post endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources :meals, only: [:index] do
         get 'foods',             to: 'meal_foods#index'
         get 'foods/:food_id',    to: 'meal_foods#show'
-        post 'foods',            to: 'meal_foods#create'
+        post 'foods/:food_id',            to: 'meal_foods#create'
         delete 'foods/:food_id', to: 'meal_foods#destroy'
       end
     end
